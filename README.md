1b41f2fbc7cc71edb91c4644922adeeb7c1786a1
Panesur <surajneupane02@gmail.com>
2024-08-30 20:13:56 +1000

    added CategoriesItems

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# react_projectwork
+# react_projectwork APP
+
+Designed and developed by Suraj Neupane
+
+
+### Technology Used
+= React
+= Vite
+= Tailwind
+= Vercel
+
+Visit the app at {https://react-projectwork-y52b.vercel.app/} (https://react-projectwork-y52b.vercel.app/)
--- react_e_commerce_app/src/App.jsx
+++ react_e_commerce_app/src/App.jsx
@@ -1,11 +1,16 @@
+import Categories from "./components/Categories"
 
 function App() {
 
   return (
-    <>
+    <div>
+     < Categories title= "Shop by Categories" linkUrl="#" linktext="Browse all Categories"/>
+  
+
+
+    </div>
       
-    </>
-  )
+    )
 }
 
 export default App
